36cf0e8de6f14cdd8d536a173eb2755c3b0dde49
renee <156127959+reneexie01@users.noreply.github.com>
2024-12-26 12:16:40 +1100

    Added ships to playerBoard visually

--- src/index.js
+++ src/index.js
@@ -3,6 +3,89 @@ const player1Board = document.querySelector('.player1-board');
 
 generateBoard(10);
 
+// Carrier
+const carrierSubmit = document.querySelector('.carrier-submit');
+const carrierCoordinateRow = document.querySelector('.player1-carrier-row');
+const carrierCoordinateColumn = document.querySelector('.player1-carrier-column');
+
+carrierSubmit.addEventListener('click', () => {
+    const startingRow = parseInt(carrierCoordinateRow.value);
+    const startingColumn = parseInt(carrierCoordinateColumn.value);
+    const direction = document.querySelector('#directions').value;
+
+    const coordinatesArray = Gameboard.generateCarrier(startingRow, startingColumn, direction).shipValidity.coordinates;
+    shipClass(coordinatesArray);
+
+    })
+
+// Battleship
+const battleshipSubmit = document.querySelector('.battleship-submit');
+const battleshipCoordinateRow = document.querySelector('.player1-battleship-row');
+const battleshipCoordinateColumn = document.querySelector('.player1-battleship-column');
+
+battleshipSubmit.addEventListener('click', () => {
+    const startingRow = parseInt(battleshipCoordinateRow.value);
+    const startingColumn = parseInt(battleshipCoordinateColumn.value);
+    const direction = document.querySelector('#directions').value;
+
+    const coordinatesArray = Gameboard.generateBattleship(startingRow, startingColumn, direction).shipValidity.coordinates;
+    shipClass(coordinatesArray);
+
+    })
+    
+// Cruiser
+const cruiserSubmit = document.querySelector('.cruiser-submit');
+const cruiserCoordinateRow = document.querySelector('.player1-cruiser-row');
+const cruiserCoordinateColumn = document.querySelector('.player1-cruiser-column');
+
+cruiserSubmit.addEventListener('click', () => {
+    const startingRow = parseInt(cruiserCoordinateRow.value);
+    const startingColumn = parseInt(cruiserCoordinateColumn.value);
+    const direction = document.querySelector('#directions').value;
+
+    const coordinatesArray = Gameboard.generateCruiser(startingRow, startingColumn, direction).shipValidity.coordinates;
+    shipClass(coordinatesArray);
+
+    })
+
+// Submarine
+const submarineSubmit = document.querySelector('.submarine-submit');
+const submarineCoordinateRow = document.querySelector('.player1-submarine-row');
+const submarineCoordinateColumn = document.querySelector('.player1-submarine-column');
+
+submarineSubmit.addEventListener('click', () => {
+    const startingRow = parseInt(submarineCoordinateRow.value);
+    const startingColumn = parseInt(submarineCoordinateColumn.value);
+    const direction = document.querySelector('#directions').value;
+
+    const coordinatesArray = Gameboard.generateSubmarine(startingRow, startingColumn, direction).shipValidity.coordinates;
+    shipClass(coordinatesArray);
+
+    })
+
+// Destroyer
+const destroyerSubmit = document.querySelector('.destroyer-submit');
+const destroyerCoordinateRow = document.querySelector('.player1-destroyer-row');
+const destroyerCoordinateColumn = document.querySelector('.player1-destroyer-column');
+
+destroyerSubmit.addEventListener('click', () => {
+    const startingRow = parseInt(destroyerCoordinateRow.value);
+    const startingColumn = parseInt(destroyerCoordinateColumn.value);
+    const direction = document.querySelector('#directions').value;
+
+    const coordinatesArray = Gameboard.generateDestroyer(startingRow, startingColumn, direction).shipValidity.coordinates;
+    shipClass(coordinatesArray);
+
+    })
+
+function shipClass(coordinatesArray) {
+    coordinatesArray.forEach((coordinate) => {
+        const id = `${coordinate[0]}-${coordinate[1]}`
+        const coordinateSelector = document.getElementById(`${id}`);
+        coordinateSelector.classList.add('ship');
+    })
+}
+
 function generateBoard(num) {
     for (let i = 0; i < num; i++) {
         const row = document.createElement('div');
@@ -11,54 +94,12 @@ function generateBoard(num) {
         for (let j = 0; j < num; j++) {
             const column = document.createElement('div');
             column.classList.add('column');
-            column.setAttribute('coordinates',`(${i},${j})`);
-            player1Board.appendChild(column);
+            column.setAttribute('id', `${i}-${j}`);
+            row.appendChild(column); 
         }
     }
 }
 
-const createPlayer = document.createElement('button');
-createPlayer.classList.add('player1-button')
-
-createPlayer.innerHTML = 'Create player'
-player1.appendChild(createPlayer);
-
-createPlayer.addEventListener('click', () => {
-    const fleet = document.createElement('div');
-    fleet.classList.add('player1-fleet')
-    
-    // Carrier
-    const carrier = document.createElement('div');
-    carrier.classList.add('player1-carrier')
-    const carrierH1 = document.createElement('h1');
-    carrierH1.innerHTML = 'Carrier'
-
-    const carrierRowH2 = document.createElement('h2');
-    carrierRowH2.innerHTML = 'Row: '
-    const carrierRow = document.createElement('input');
-    carrierRow.classList.add('player1-carrier-row')
-    
-    const carrierColumnH2 = document.createElement('h2');
-    carrierColumnH2.innerHTML = 'Column: '
-    const carrierColumn = document.createElement('input');
-    carrierColumn.classList.add('player1-carrier-column');
-    
-    const carrierDirectionH2 = document.createElement('h2');
-    carrierDirectionH2.innerHTML = 'Direction: ';
-    const carrierDirection = document.createElement('input');
-    carrierDirection.classList.add('player1-carrier-direction')
-
-    player1.appendChild(fleet)
-    fleet.appendChild(carrier);
-    carrier.appendChild(carrierH1);
-    carrierH1.appendChild(carrierRowH2);
-    carrierRowH2.appendChild(carrierRow);
-    carrierH1.appendChild(carrierColumnH2);
-    carrierColumnH2.appendChild(carrierColumn);
-    carrierH1.appendChild(carrierDirectionH2);
-    carrierDirectionH2.appendChild(carrierDirection);
-
-})
 
 import { Gameboard } from "./gameboard.js";
 import './styles.css';
